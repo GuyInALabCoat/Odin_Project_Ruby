d96d5074cb5c20fb2c3dd40593b3448fe53b204c
Dmytro <dmytropolitov@gmail.com>
2017-06-25 20:57:50 -0400

    Rewrote test methods to conform to ruby style guidelines
    Wrote test methods to use do...end syntax for multiline blocks
    	as opposed to {...}

--- Advanced_Building_Blocks/tc_enumerable_methods.rb
+++ Advanced_Building_Blocks/tc_enumerable_methods.rb
@@ -19,45 +19,60 @@ class TestEnumerableMethods < Test::Unit::TestCase
 		test_array = []
 		hash = Hash.new
 
-		[13, 15, 17, 19].my_each_with_index {|element, index| \
-									test_array << element if index.even? }
+		[13, 15, 17, 19].my_each_with_index do |element, index| 
+			test_array << element if index.even?
+		end
 		
-		%w(cat dog wombat).my_each_with_index {|item, index|\
-							hash[item] = index }
+		%w(cat dog wombat).my_each_with_index do |item, index|
+			hash[item] = index
+		end
 
 		assert_equal([13, 17], test_array)
 		assert_equal({"cat"=>0, "dog"=>1, "wombat"=>2}, hash)
 	end
 
 	def test_my_select
-		assert_equal([1, 2], [1, 2, "hello", [], :item].my_select \
-													{|item| item.is_a?(Integer)})
+		assert_equal([1, 2], [1, 2, "hello", [], :item].my_select do
+			|item| item.is_a?(Integer)
+		end )
 
 		assert_equal([2, 4], [1,2,3,4,5].my_select { |num| num.even? })
 	end
 
 	def test_my_all?
-		assert_equal(true, [2, 4, 6, 8].my_all? {|item| item%2==0})
-		assert_equal(true, %w[ant bear cat].my_all? { |word| \
-																								word.length >= 3})
-		assert_equal(false, %w[ant bear cat].my_all? { |word| \
-																								 word.length >= 4 })
+		assert_equal(true, [2, 4, 6, 8].my_all? { |item| item.even? })
+		assert_equal(true, %w[ant bear cat].my_all? do |word| 
+			word.length >= 3
+		end )
+
+		assert_equal(false, %w[ant bear cat].my_all? do |word| 
+			word.length >= 4 
+		end )
+
 		assert_equal(false, [nil, true, 99].my_all?)
 	end
 
 	def test_my_any?
-		assert_equal(true, ["A", [], 13, [], true].my_any? {|item|\
-																	item.is_a?(String)})
-		assert_equal(true, %w[ant bear cat].my_any? {|word| \
-																								word.length >= 3})
+		assert_equal(true, ["A", [], 13, [], true].my_any? do |item|
+			item.is_a?(String)
+		end )
+
+		assert_equal(true, %w[ant bear cat].my_any? do |word| 
+			word.length >= 3
+		end )
+		
 		assert_equal(true, [nil, true, 99].my_any?)
 	end
 
 	def test_my_none?
-		assert_equal(false, [15, 20, [], true, "buy"].my_none? \
-														{|item| item.is_a?(String)})
-		assert_equal(true, %w{ant bear cat}.my_none? { |word| \
-										word.length == 5 })
+		assert_equal(false, [15, 20, [], true, "buy"].my_none? do |item| 
+			item.is_a?(String)
+		end )
+		
+		assert_equal(true, %w{ant bear cat}.my_none? do |word| 
+			word.length == 5 
+		end )
+
 		assert_equal(true, [].my_none?)
 		assert_equal(true, [nil].my_none?)
 		assert_equal(true, [nil, false].my_none?)
@@ -71,14 +86,18 @@ class TestEnumerableMethods < Test::Unit::TestCase
 
 	def test_my_map
 		test_proc = Proc.new { |arg1| arg1 * 3.0 / 5.0 }
-		assert_equal([1, 3, 3, 4], [3, 6, 5, 8].my_map {|item| \
-														item * 3 / 5 })
+		
+		assert_equal([1, 3, 3, 4], [3, 6, 5, 8].my_map do |item|
+			item * 3 / 5 
+		end )
+
 		assert_equal([1.8, 3.6, 3.0, 4.8], [3, 6, 5, 8].my_map(&test_proc))
 	end
 
 	def test_my_inject
-		assert_equal(120, (1..15).to_a.inject {|running_total, item| \
-											running_total + item })
+		assert_equal(120, (1..15).to_a.inject do |running_total, item|
+			running_total + item 
+		end )
 	end
 
 	def test_multiply_els
