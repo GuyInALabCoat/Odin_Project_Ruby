5294d80485747c82fbba75a022148e7a4c5a6ed9
Dmytro <dmytropolitov@gmail.com>
2017-06-25 20:47:04 -0400

    Initial commit: test overriden Enumerate methods in enumerable_methods.rb

---
+++ Advanced_Building_Blocks/tc_enumerable_methods.rb
@@ -0,0 +1,88 @@
+# File: tc_enumerable_methods.rb
+
+require_relative "enumerable_methods"
+require "test/unit"
+
+class TestEnumerableMethods < Test::Unit::TestCase
+
+	def setup
+		@instance = Class.new{ include Enumerable }.new
+	end
+
+	def test_my_each
+		test_array = []
+		[1,2,3].my_each {|element| test_array << element**3 }
+		assert_equal([1,8,27], test_array)
+	end
+
+	def test_my_each_with_index
+		test_array = []
+		hash = Hash.new
+
+		[13, 15, 17, 19].my_each_with_index {|element, index| \
+									test_array << element if index.even? }
+		
+		%w(cat dog wombat).my_each_with_index {|item, index|\
+							hash[item] = index }
+
+		assert_equal([13, 17], test_array)
+		assert_equal({"cat"=>0, "dog"=>1, "wombat"=>2}, hash)
+	end
+
+	def test_my_select
+		assert_equal([1, 2], [1, 2, "hello", [], :item].my_select \
+													{|item| item.is_a?(Integer)})
+
+		assert_equal([2, 4], [1,2,3,4,5].my_select { |num| num.even? })
+	end
+
+	def test_my_all?
+		assert_equal(true, [2, 4, 6, 8].my_all? {|item| item%2==0})
+		assert_equal(true, %w[ant bear cat].my_all? { |word| \
+																								word.length >= 3})
+		assert_equal(false, %w[ant bear cat].my_all? { |word| \
+																								 word.length >= 4 })
+		assert_equal(false, [nil, true, 99].my_all?)
+	end
+
+	def test_my_any?
+		assert_equal(true, ["A", [], 13, [], true].my_any? {|item|\
+																	item.is_a?(String)})
+		assert_equal(true, %w[ant bear cat].my_any? {|word| \
+																								word.length >= 3})
+		assert_equal(true, [nil, true, 99].my_any?)
+	end
+
+	def test_my_none?
+		assert_equal(false, [15, 20, [], true, "buy"].my_none? \
+														{|item| item.is_a?(String)})
+		assert_equal(true, %w{ant bear cat}.my_none? { |word| \
+										word.length == 5 })
+		assert_equal(true, [].my_none?)
+		assert_equal(true, [nil].my_none?)
+		assert_equal(true, [nil, false].my_none?)
+	end
+
+	def test_my_count
+		assert_equal(4, [3, 4, 1, 2].my_count)
+		assert_equal(2, [1, 2, 4, 2].my_count(2))
+		assert_equal(3, [1, 2, 4, 2].my_count { |x| x%2==0 })
+	end
+
+	def test_my_map
+		test_proc = Proc.new { |arg1| arg1 * 3.0 / 5.0 }
+		assert_equal([1, 3, 3, 4], [3, 6, 5, 8].my_map {|item| \
+														item * 3 / 5 })
+		assert_equal([1.8, 3.6, 3.0, 4.8], [3, 6, 5, 8].my_map(&test_proc))
+	end
+
+	def test_my_inject
+		assert_equal(120, (1..15).to_a.inject {|running_total, item| \
+											running_total + item })
+	end
+
+	def test_multiply_els
+		assert_equal(40, @instance.multiply_els([2,4,5]))
+	end
+
+end
