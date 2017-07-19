2f5d136c7f21cc351983a971433e600ec83cb012
Dmytro <dmytropolitov@gmail.com>
2017-07-18 20:08:46 -0400

    Refactored user interface to allow user to set code

--- OOP/Mastermind/logic/main.rb
+++ OOP/Mastermind/logic/main.rb
@@ -13,37 +13,90 @@ class Main
 	end
 
 	def run
-		win_flag = false
-		@mastermind.generate_code
+	puts "Would you like to create the code or guess it? [ create | guess ]"
 
-		while @turns > 0
-			puts "Enter your 4 digit guess, from 1 to 6"
-
-			guess = gets.chomp
-	
-			if guess.length != 4 || guess !~ /[1-6]{4}/
-				puts "Your guess must be four digits long and only contain"
-				puts "numbers from 1 to 6. Try again." 
-			
-			elsif @mastermind.guess(guess)
-				win_flag = true
+		while user_input = gets.chomp.downcase
+			if user_input == "create" || user_input == "guess"
 				break
-
 			else
-				@turns -= 1
-				puts "#{@turns} guesses remaining"
+				puts "Please enter either 'create' or 'guess'"
 			end
 		end
 
-		if win_flag
-			game_won
+		if user_input == "create"
+			player_sets_code
+			computer_guess
 		else
-			game_lost
+			@mastermind.generate_code
+			player_guesses
 		end
 	end
 
 	private
-		
+
+		def player_sets_code
+			puts "Please enter a 4-digit code using any combinations of numbers"
+			puts "between 1 and 6"
+
+			while user_input = gets.chomp
+				if user_input =~ /[1-6]{4}/
+					@mastermind.set_code(user_input)
+					break
+				else
+					puts "Please enter a 4-digit code using the numbers 1-6"
+				end
+			end
+		end
+
+		def player_guesses
+			win_flag = false
+
+			while @turns > 0
+				puts "Enter your 4 digit guess, from 1 to 6"
+
+				guess = gets.chomp
+	
+				if guess.length != 4 || guess !~ /[1-6]{4}/
+					puts "Your guess must be four digits long and only contain"
+					puts "numbers from 1 to 6. Try again." 
+			
+				elsif @mastermind.guess(guess)
+					win_flag = true
+					break
+
+				else
+					@turns -= 1
+					puts "#{@turns} guesses remaining"
+				end
+			end
+
+			check_win(win_flag)
+		end
+
+		def computer_guesses
+			win_flag = false
+
+			while @turns > 0
+
+				if @mastermind.guess(guess)
+					win_flag = true
+					break
+
+				else
+					@turns -= 1
+				end
+			end
+			check_win(win_flag)
+		end
+
+		def check_win(flag)
+			if flag
+				game_won
+			else
+				game_lost
+			end
+		end
+
 		def game_won
 			puts "Congratulations, you won!"
 			play_again
