34243944a9ce16da843d01d1a7390685b2058aef
Dmytro <dmytropolitov@gmail.com>
2017-07-31 17:12:30 -0400

    refactored to use csv parser and access name + zipcode

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,13 +1,21 @@
 #!/usr/bin/ruby
 
-puts "EventManager Initialized!"
+require "csv"
+
+def clean_zipcode(zipcode)
+	zipcode.to_s.rjust(5, "0")[0..4]
+end
+
+puts "EventManager Initialized."
 
 path = File.expand_path('../../event_attendees.csv', __FILE__)
 
-lines = File.readlines(path)
-lines.each_with_index do |line, index|
-	next if index == 0
-	columns = line.split(",")
-	name = columns[2]
-	puts name
+contents = CSV.open(path, headers: true, header_converters: :symbol)
+
+contents.each do |row|
+	name = row[:first_name]
+
+	zipcode = clean_zipcode(row[:zipcode])
+
+	puts "#{name} #{zipcode}"
 end
