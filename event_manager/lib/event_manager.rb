11c4afeb50aa5bb5f2a50da75f8a4d377a03e610
Dmytro <dmytropolitov@gmail.com>
2017-07-31 16:51:23 -0400

    modified event manager to output first names of attendees

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -5,6 +5,9 @@ puts "EventManager Initialized!"
 path = File.expand_path('../../event_attendees.csv', __FILE__)
 
 lines = File.readlines(path)
-lines.each do |line|
-	puts line
+lines.each_with_index do |line, index|
+	next if index == 0
+	columns = line.split(",")
+	name = columns[2]
+	puts name
 end
