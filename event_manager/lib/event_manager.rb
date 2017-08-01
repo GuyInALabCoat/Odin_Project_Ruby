8d87c024caf70e20e6cbad4070bf55f3174b47a9
Dmytro <dmytropolitov@gmail.com>
2017-07-31 22:48:44 -0400

    Added API functionality to get legislators by zipcode
    Uses the sunlight-congress library, method can query legislator names
    using zipcode.
    Names are collected into a string and injected into an html template.

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,21 +1,39 @@
 #!/usr/bin/ruby
 
-require "csv"
+require 'csv'
+require 'sunlight/congress'
+
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
 def clean_zipcode(zipcode)
 	zipcode.to_s.rjust(5, "0")[0..4]
 end
 
+def legislators_by_zipcode(zipcode)
+	legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+
+	legislator_names = legislators.collect do |legislator|
+		"#{legislator.first_name} #{legislator.last_name}"
+	end
+
+	legislator_names.join(", ")
+end
+
 puts "EventManager Initialized."
 
-path = File.expand_path('../../event_attendees.csv', __FILE__)
+contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
 
-contents = CSV.open(path, headers: true, header_converters: :symbol)
+template_letter = File.read("form_letter.html")
 
 contents.each do |row|
 	name = row[:first_name]
 
 	zipcode = clean_zipcode(row[:zipcode])
 
-	puts "#{name} #{zipcode}"
+	legislators = legislators_by_zipcode(zipcode)
+	
+	personal_letter = template_letter.gsub('FIRST_NAME', name)
+	personal_letter.gsub!('LEGISLATORS', legislators)
+	
+	puts personal_letter
 end
